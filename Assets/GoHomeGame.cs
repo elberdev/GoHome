cb3fdb15fbde5d6be9dc54c8670c3c8805e3c482
elberdev <elberdev@gmail.com>
2016-05-12 22:38:17 -0400

    Initial setup of Main scene and GoHomeGame script

---
+++ Assets/GoHomeGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoHomeGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Go Home!\n");
+		print("A game where you need to find your way home.\n");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
