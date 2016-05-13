fc8ddf18013ed8a8877260146070d67a8ba281e5
elberdev <elberdev@gmail.com>
2016-05-13 19:43:22 -0400

    added a real 2d coordinate system for the locations and use vector arithmetic to move the player

--- Assets/GoHomeGame.cs
+++ Assets/GoHomeGame.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class GoHomeGame : MonoBehaviour {
 
 	private int numberOfSteps = 0;
-	private float metersFromHome = 0.0f;
+	private Vector2 homeLocation = new Vector2(2.0f, 3.0f);
+	private Vector2 playerLocation = new Vector2(5.0f, 1.0f);
 
 	// Use this for initialization
 	void Start () {
@@ -13,24 +14,31 @@ public class GoHomeGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) {
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			// add one unit up
+			playerLocation += Vector2.up;
+			UpdateStatus ();
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			// add one unit down, you get the idea
+			playerLocation += Vector2.down;
 			UpdateStatus ();
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			metersFromHome++;
+			playerLocation += Vector2.right;
 			UpdateStatus ();
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			metersFromHome--;
+			playerLocation += Vector2.left;
 			UpdateStatus ();
 		}
 	}
 
 	void PrintWelcomeMessage() {
 		print ("Welcome to Go Home!\n");
-		print("A game where you need to find your way home.\n");
+		print ("A game where you need to find your way home.\n");
 	}
 
 	void UpdateStatus() {
+		Vector2 distance = homeLocation - playerLocation;
 		numberOfSteps++;
-		print ("After " + numberOfSteps + " steps you are " + metersFromHome + " meters away from home.\n");
+		print ("After " + numberOfSteps + " steps you are " + distance + " meters away from home.\n");
 	}
 }
