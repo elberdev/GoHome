d5be60074c6797ed940f35332135041e4ebefcb5
elberdev <elberdev@gmail.com>
2016-05-14 07:46:23 -0400

    added game over behavior (will no longer respond to key presses)

--- Assets/GoHomeGame.cs
+++ Assets/GoHomeGame.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoHomeGame : MonoBehaviour {
+public class GoHomeGame : MonoBehaviour
+{
 
 	private int numberOfSteps = 0;
-	private Vector2 homeLocation = new Vector2(2.0f, 3.0f);
-	private Vector2 playerLocation = new Vector2(5.0f, 1.0f);
+	private Vector2 homeLocation = new Vector2 (2.0f, 3.0f);
+	private Vector2 playerLocation = new Vector2 (5.0f, 1.0f);
+	private bool gameOver = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		PrintWelcomeMessage ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		if (gameOver) {
+			return;
+		}
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			// add one unit up
 			playerLocation += Vector2.up;
@@ -31,16 +38,18 @@ public class GoHomeGame : MonoBehaviour {
 		}
 	}
 
-	void PrintWelcomeMessage() {
+	void PrintWelcomeMessage ()
+	{
 		print ("Welcome to Go Home!\n");
 		print ("A game where you need to find your way home.\n");
 	}
 
-	void UpdateStatus() {
+	void UpdateStatus ()
+	{
 		Vector2 distance = homeLocation - playerLocation;
 		bool gameOver = distance.magnitude == 0;
 		numberOfSteps++;
-		if (gameOver == true) {
+		if (gameOver) {
 			print ("You win! You made it back to your stinky home in " + numberOfSteps + "!\n");
 		} else {
 			print ("After " + numberOfSteps + " steps you are " + distance.magnitude + " meters away from home.\n");
